12d35e998f5a800618fdb84f762bb883fc74e5cd
callumjg <callumcancreate@gmail.com>
2022-11-11 22:20:17 +1000

    update for 2022

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
 export class BannerComponent implements OnInit {
-
-  constructor() { }
+  year: string = '';
 
   ngOnInit(): void {
+    const date = new Date();
+    this.year = String(date.getFullYear());
   }
-
 }
